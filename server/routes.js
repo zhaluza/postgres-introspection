d0463f421695b1d07d3270440f397398c8c6bd45
Zac H <zac.haluza@gmail.com>
2020-04-06 23:32:48 -0400

    can now get foreign key and default value info

--- server/routes.js
+++ server/routes.js
@@ -25,17 +25,24 @@ router.get('/schemas', async (req, res) => {
     tableList.push(table);
   }
 
-  // for each table, query all the details
+  // for each table, get all column details
   let schemaArray = [];
   for (let i = 0; i < tableList.length; i++) {
     const table = tableList[i].table_name;
     console.log(table);
-    const displayTableData = `select table_name, column_name, data_type, character_maximum_length, is_nullable
+    const displayTableData = `select table_name, column_name, data_type, column_default, character_maximum_length, is_nullable
     from INFORMATION_SCHEMA.COLUMNS where table_name = '${table}';`;
     schemaData = await db.query(displayTableData);
     schemaArray.push(schemaData.rows);
   }
   res.status(200).send(schemaArray);
+});
+
+// Returns list of primary and foreign keys
+router.get('/fk', async (req, res) => {
+  const displayfk = `SELECT conrelid::regclass AS table_from, conname, pg_get_constraintdef(oid) FROM pg_constraint WHERE  contype IN ('f', 'p ') AND connamespace = 'public'::regnamespace  -- your schema here ORDER  BY conrelid::regclass::text, contype DESC;`;
+  const { rows } = await db.query(displayfk);
+  res.status(200).send(rows);
 });
 
 module.exports = router;
